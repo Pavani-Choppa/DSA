bc31b3f20cee31eb4d51e1ffb7f0495518392034
Pavani-Choppa <pavanicm011@gmail.com>
2025-08-05 20:24:47 +0530

    merge dort is completed

--- sorting.cpp
+++ sorting.cpp
@@ -24,7 +24,7 @@ using namespace std;
 // void bubble_sort(vector<int>& arr, int n)
 // {
 //     // int n = arr.size();
-//     for(int i = n-1; i >= 0; i--){
+//     for(int i = n-1; i > 0; i--){
 //         for(int j = 0; j < i; j++){
 //             if(arr[j] > arr[j+1])
 //                 swap(arr[j], arr[j+1]);
@@ -38,29 +38,80 @@ using namespace std;
 // }
 
 
-void insertion_sort(vector<int> & arr, int n)
+// void insertion_sort(vector<int> & arr, int n)
+// {
+//     for(int i = 0; i <= n-1; i++)
+//     {
+//         int j = i;
+//         while(j > 0 && arr[j - 1] > arr[j]){
+//             int temp = arr[j-1];
+//             arr[j-1] = arr[j];
+//             arr[j] = temp;
+//             j--;
+//         }
+//     }
+//     cout<<endl<<"The array after Insertion Sorting is : "<<endl;
+//     for(int i = 0; i < n; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+void merge(vector<int> & arr, int low , int mid , int high)
 {
-    for(int i = 0; i <= n-1; i++)
+    vector<int> temp;
+    int left = low;
+    int right = mid +1;
+
+    while(left <= mid && right <= high)
     {
-        int j = i;
-        while(j > 0 && arr[j - 1] > arr[j]){
-            int temp = arr[j-1];
-            arr[j-1] = arr[j];
-            arr[j] = temp;
-            j--;
+        if(arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
         }
     }
-    cout<<endl<<"The array after Insertion Sorting is : "<<endl;
-    for(int i = 0; i < n; i++)
+
+    while(left <= mid)
     {
-        cout<<arr[i]<<" ";
+        temp.push_back(arr[left]);
+        left++;
     }
+
+    while(right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+
+   
+}
+
+ void merge_sort(vector<int> & arr, int low , int high)
+{
+    if(low >= high) return;
+    int mid = (low + high)/2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+    
 }
 
 int main(){
     int n;
     cin >> n;
     vector<int> arr(n);
+
     for(int i = 0; i  < n; i++)
         cin >> arr[i];
 
@@ -70,7 +121,14 @@ int main(){
     cout << endl;
     // selection_sort(arr,n);
     // bubble_sort(arr,n);
-    insertion_sort(arr,n);
+    // insertion_sort(arr,n);
+    merge_sort(arr,0,n-1);
+
+    cout<<endl<<"The array after Merge Sorting is : "<<endl;
+    for(int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
     
