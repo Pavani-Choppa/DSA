6771c9a353d7a24d72d5106920ec57fd2e26a41c
Pavani-Choppa <pavanicm011@gmail.com>
2025-08-08 22:17:51 +0530

    to check given array is sorted or not is done

---
+++ is_sorted_array.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isArraySorted(int arr[], int n)
+{
+    /*BruteForce Method*/
+    // for (int i = 0; i < n; i++) 
+    // {
+    //     for (int j = i + 1; j < n; j++) 
+    //     {
+    //         if (arr[j] < arr[i])
+    //             return false;
+    //     }
+    // }
+
+    // return true;
+
+    
+
+    /*Optimal mehod*/
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] < arr [i-1])
+        {
+            return false;
+        }
+       
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    //  printf("%s",isArraySorted(arr,n) ? "Array is Sorted" : "Array is not sorted");
+   
+    if (isArraySorted(arr, n))
+        cout << "Array is sorted\n";
+    else
+        cout << "Array is NOT sorted\n";
+}
