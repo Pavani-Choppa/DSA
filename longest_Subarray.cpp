93e1f0b9d69bf1ef6f0dec84dc5a6f6b732064ab
Pavani-Choppa <pavanicm011@gmail.com>
2025-08-20 23:49:25 +0530

    commiting the finding the longest subarray from the array in the better approach by using hashing

--- longest_Subarray.cpp
+++ longest_Subarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-int logenstSubarray(int arr[], int n, int k){
+int logenstSubarray(vector<int> &arr, int n, int k){
    
     /*
         BruteForce Method 
@@ -25,27 +25,46 @@ int logenstSubarray(int arr[], int n, int k){
     // }
     // return maxLen;
 
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for(int i =0; i < arr.size(); i++)
+    {
+        sum =arr[i];
+        if(sum == k){
+            maxLen = max(maxLen, i+1);
+        }
+        long long rem = sum-k;
+        if(preSumMap.find(rem) != preSumMap.end()){
+            int len = i-preSumMap[rem];
+            maxLen = max(maxLen,len);
+        }
+        if(preSumMap.find(sum) == preSumMap.end()){
+            preSumMap[sum] = i;
+        }
+    }
      /*
         Optimal Approach
         Time Complexity = O(N)
         Space Complexity = O(1)
     */
 
-    int maxLen = 0, sum = 0, left = 0, right = 0;
+    // int maxLen = 0, sum = 0, left = 0, right = 0;
     
-    while(right < n){
-        sum += arr[right];
+    // while(right < n){
+    //     sum += arr[right];
 
-        while(sum > k && left <= right){
-            sum -= arr[left];
-            left++;
-        }
+    //     while(sum > k && left <= right){
+    //         sum -= arr[left];
+    //         left++;
+    //     }
 
-        if(sum == k){
-            maxLen = max(maxLen,right - left +1);
-        }
-        right++;
-    }
+    //     if(sum == k){
+    //         maxLen = max(maxLen,right - left +1);
+    //     }
+    //     right++;
+    // }
     return maxLen;
 }
 
@@ -53,7 +72,7 @@ int main()
 {
     int n,k;
     cin>>n>>k;
-    int arr[n];
+    vector<int> arr(n);
     for(int i = 0; i < n; i++) cin>>arr[i];
     cout<<"The Longest Subarray Lenght is : "<<logenstSubarray(arr,n,k);
 }
