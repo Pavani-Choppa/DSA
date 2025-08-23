a75b6ba6fca4fdee155be27f19bddd7c3dcd2cfd
Pavani-Choppa <pavanicm011@gmail.com>
2025-08-23 23:54:32 +0530

    sorting the array of zeros,ones,twos

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -1,18 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string TwoSumProblem(int arr[], int n, int target)
-{
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j <n; j++){
-            if(i == j) continue;
-            if(arr[i] + arr[j] == target)
-            {
-                return "the taget is met";
+ /*
+        BruteForce Method 
+        Time Complexity = O(N^2)
+        Space Complexity = O(N)
+    */
+   
+// string TwoSumProblem(int arr[], int n, int target)
+// {
+//     for(int i = 0; i < n; i++){
+//         for(int j = i+1; j <n; j++){
+//             // if(i == j) continue;
+//             if(arr[i] + arr[j] == target)
+//             {
+//                 return "the taget is met";
+//             }
+//         }
+//     }
+//     return "target is not met";
+// }
+
+
+/*
+        better solution
+        Time Complexity = O(N LOG N)
+        Space Complexity = O(N)
+    */
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        for(int i =0; i < nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+            if(mpp.find(more) != mpp.end()){
+                return{mpp[more],i};
             }
+            mpp[a] = i;
         }
+        return {-1,-1};
     }
-    return "target is not met";
+
+
+/*
+       OPTIMAL METHOD 
+        Time Complexity = O(N)
+        Space Complexity = O(1)
+    */
+string read(int n, vector<int> book, int target)
+{
+    // Write your code here.
+    sort(book.begin(),book.end());
+    int left = 0 , right = n-1;
+    for(int i = 0; i < n ;i++){
+        int sum = book[left] + book[right];
+        if(sum == target) return "YES";
+        if(sum < target) left++;
+        else right--;
+    }
+    return "NO";
 }
 
 int main()
